b10b44cdfe19bd09a3104c084eaacee3c6581269
Mathieu Lamiot <mathieu.lamiot@lydia-app.com>
2023-03-15 08:41:42 +0100

    Initialize repo Initialize tp_package with first function and first test

---
+++ tests/test_tp_package/test_functions.py
@@ -0,0 +1,15 @@
+import pytest
+from tp_package.functions import add_one
+
+
+# --- build_api_url - Unit Tests ---
+# Refer to https://www.vcalc.com/wiki/vCalc/Haversine+-+Distance to check computations
+@pytest.mark.parametrize("input, expected", [
+    (0,1),
+])
+def test_add_one(input,expected):
+  # Arrange
+  # Act
+  res = add_one(input)
+  # Assert
+  assert(expected==res)
